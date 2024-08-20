4e7587ccace6b14f98f226e02239dc4901d77966
Duranto96 <durantonella96@gmail.com>
2024-08-20 01:18:57 -0300

    solucion-click

--- eventos.js
+++ eventos.js
@@ -5,3 +5,10 @@ const miDiv = document.getElementById("miDiv");
 miDiv.addEventListener("click", function () {
   alert("Hola! Soy el div");
 });
+
+// Solución para que no se ejecute el onclick
+const miBtn = document.getElementById("miBtn");
+miBtn.addEventListener("click", function (event) {
+  // Detener la propagación para que no se ejecute el onclick
+  event.stopPropagation();
+});
