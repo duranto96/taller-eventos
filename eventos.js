f61be0d34c7dadff2f9b438e95a88d7dada5a303
Duranto96 <durantonella96@gmail.com>
2024-08-20 00:50:05 -0300

    Con Js integrado

---
+++ eventos.js
@@ -0,0 +1,7 @@
+// Obtener el elemento div por su ID
+const miDiv = document.getElementById("miDiv");
+
+// Agregar el manejador de eventos para el click
+miDiv.addEventListener("click", function () {
+  alert("Hola! Soy el div");
+});
